4407ffc37a2240b0b22f5e67f26ab29007c29484
qingfeng346 <250178206@qq.com>
2014-09-23 11:36:28 +0800

    userdata 支持 {get; set;} 函数 直接赋值

--- Scorpio/ScriptUserdata.cs
+++ Scorpio/ScriptUserdata.cs
@@ -9,9 +9,34 @@ namespace Scorpio
     //语言数据
     public class ScriptUserdata : ScriptObject
     {
+        private class Field
+        {
+            public string name;
+            public Type fieldType;
+            public FieldInfo field;
+            public MethodInfo getMethod;
+            public MethodInfo setMethod;
+            public object GetValue(object obj)
+            {
+                if (field != null)
+                    return field.GetValue(obj);
+                else if (getMethod != null)
+                    return getMethod.Invoke(obj, null);
+                throw new ScriptException("变量 [" + name + "] 不支持GetValue"); 
+            }
+            public void SetValue(object obj, object val)
+            {
+                if (field != null)
+                    field.SetValue(obj, val);
+                else if (setMethod != null)
+                    setMethod.Invoke(obj, new object[] { val });
+                else
+                    throw new ScriptException("变量 [" + name + "] 不支持SetValue"); 
+            }
+        }
         public object Value { get; private set; }
         public Type ValueType { get; private set; }
-        private Dictionary<string, FieldInfo> FieldInfos = new Dictionary<string, FieldInfo>();
+        private Dictionary<string, Field> FieldInfos = new Dictionary<string, Field>();
         private Dictionary<string, ScriptFunction> Functions = new Dictionary<string, ScriptFunction>();
         public ScriptUserdata(object value)
         {
@@ -19,19 +44,45 @@ namespace Scorpio
             Value = value;
             ValueType = (Value is Type) ? (Type)value : value.GetType();
         }
-        private FieldInfo GetField(string strName)
+        private Field GetField(string strName)
         {
             if (FieldInfos.ContainsKey(strName))
                 return FieldInfos[strName];
+            Field field = new Field();
+            field.name = strName;
             FieldInfo info = ValueType.GetField(strName);
-            if (info != null) FieldInfos.Add(strName, info);
-            return info;
+            if (info != null)
+            {
+                field.field = info;
+                field.fieldType = info.FieldType;
+                FieldInfos.Add(strName, field);
+                return field;
+            }
+            MethodInfo method = ValueType.GetMethod("get_" + strName);
+            if (method != null)
+            {
+                field.getMethod = method;
+                field.fieldType = method.ReturnType;
+                field.setMethod = ValueType.GetMethod("set_" + strName);
+                FieldInfos.Add(strName, field);
+                return field;
+            }
+            method = ValueType.GetMethod("set_" + strName);
+            if (method != null)
+            {
+                field.setMethod = method;
+                field.fieldType = method.GetParameters()[0].ParameterType;
+                field.getMethod = ValueType.GetMethod("get_" + strName);
+                FieldInfos.Add(strName, field);
+                return field;
+            }
+            return null;
         }
         public ScriptObject GetValue(string strName)
         {
             if (Functions.ContainsKey(strName))
                 return Functions[strName];
-            FieldInfo field = GetField(strName);
+            Field field = GetField(strName);
             if (field != null) return ScriptObject.CreateObject(field.GetValue(Value));
             ScriptFunction func = new ScriptFunction("", new ScorpioMethod(ValueType, strName, Value));
             Functions[strName] = func;
@@ -39,9 +90,9 @@ namespace Scorpio
         }
         public void SetValue(string strName, ScriptObject value)
         {
-            FieldInfo field = GetField(strName);
+            Field field = GetField(strName);
             if (field == null) throw new ScriptException("Type[" + ValueType + "] 变量 [" + strName + "] 不存在");
-            field.SetValue(Value, Util.ChangeType(value, field.FieldType));
+            field.SetValue(Value, Util.ChangeType(value, field.fieldType));
         }
         public override string ToString() { return "Userdata"; }
     }
