00911bf18ffc67052ad8dbaa28d6eec82fc5864b
while <qingfeng346@126.com>
2018-02-07 14:48:11 +0800

    修改bat

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 cd Scorpio
-dotnet build -o ..\bin\win
+dotnet build -o ../bin/win
 copy /y ..\bin\win\Scorpio.dll ..\ScorpioTest\Assets\Plugins
-cd ..\ScorpioExec
-dotnet build -o ..\bin\win -r win7-x64
+cd ../ScorpioExec
+dotnet build -o ../bin/win -r win7-x64
