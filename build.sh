0c0589ccff052f3109e3bfcd3c4ad3842543c746
while <250178206@qq.com>
2018-02-07 14:45:13 +0800

    生成mac运行程序

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cd Scorpio
-dotnet build -o ..\bin\mac
-cp -rf ..\bin\mac\Scorpio.dll ..\ScorpioTest\Assets\Plugins
-cd ..\ScorpioExec
-dotnet build -o ..\bin\mac -r osx.10.11-x64
+dotnet build -o ../bin/mac
+cp -rf ../bin/mac/Scorpio.dll ../ScorpioTest/Assets/Plugins
+cd ../ScorpioExec
+dotnet build -o ../bin/mac -r osx.10.11-x64
