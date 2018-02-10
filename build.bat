58f4f33085d74768f44bd7c303f61f9960155dcb
while <qingfeng346@fengyuezhu.com>
2018-02-10 15:53:58 +0800

    修改生成环境

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 cd Scorpio
-dotnet build -c Release -o ../bin/win
+dotnet build -c release -o ../bin/win
 copy /y ..\bin\win\Scorpio.dll ..\ScorpioTest\Assets\Plugins
 cd ../ScorpioExec
-dotnet build -c Release -o ../bin/win -r win7-x64
+dotnet build -c release -o ../bin/win -r win7-x64
