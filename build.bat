fbd91e582234cc10cbf6adeea6efa0daad18f02f
while <while@while.com>
2018-03-06 18:52:34 +0800

    修改生成方式

--- build.bat
+++ build.bat
@@ -1,5 +1,2 @@
-cd Scorpio
-dotnet build -c release -o ../bin/win
-copy /y ..\bin\win\Scorpio.dll ..\ScorpioTest\Assets\Plugins
 cd ../ScorpioExec
-dotnet build -c release -o ../bin/win -r win7-x64
+dotnet publish -c release -o ../bin/win -r win7-x64
