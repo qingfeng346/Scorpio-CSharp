b0866308e2c01ae064be7169001eed6b994a4828
linyuan.yang <linyuan.yang@centurygame.com>
2018-09-26 18:37:34 +0800

    修改生成脚本

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 cd ScorpioExec
 version=1.0.0
-dotnet publish -c release -o ../bin/sco-$version-windows-x64 -r win7-x64
-dotnet publish -c release -o ../bin/sco-$version-macos-x64 -r osx.10.11-x64
-dotnet publish -c release -o ../bin/sco-$version-centos-x64 -r centos.7-x64
-dotnet publish -c release -o ../bin/sco-$version-ubuntu-x64 -r ubuntu.14.04-x64
-dotnet publish -c release -o ../bin/sco-$version-debian-x64 -r debian.8-x64
+dotnet publish -c release -o ../bin/sco-$version-win-x64 -r win-x64
+dotnet publish -c release -o ../bin/sco-$version-osx-x64 -r osx-x64
+dotnet publish -c release -o ../bin/sco-$version-linux-x64 -r linux-x64
