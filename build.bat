9e4062a146b631a101bfdab6e39ae611f5d4c3ae
linyuan.yang <linyuan.yang@centurygame.com>
2018-11-26 18:57:33 +0800

    修改 nuget 更新日志

--- build.bat
+++ build.bat
@@ -1,8 +1,10 @@
 cd ScorpioExec
-set version=1.0.1
+set version=1.0.2
 dotnet publish -c release -o ../bin/sco-%version%-win-x64 -r win-x64
 dotnet publish -c release -o ../bin/sco-%version%-osx-x64 -r osx-x64
 dotnet publish -c release -o ../bin/sco-%version%-linux-x64 -r linux-x64
+cd ../Scorpio
+dotnet pack -p:PackageVersion=%version% -o ../bin
 rd /S /Q ..\ScorpioTest\Assets\Scorpio\
 mkdir ..\ScorpioTest\Assets\Scorpio\
 xcopy /s /e ..\Scorpio\src\*.* ..\ScorpioTest\Assets\Scorpio\
