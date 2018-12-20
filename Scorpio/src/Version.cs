11bd883865aa285799191815c724b7c2f9ff227f
linyuan.yang <linyuan.yang@centurygame.com>
2018-12-20 18:19:24 +0800

    升级版本号到 1.0.4

--- Scorpio/src/Version.cs
+++ Scorpio/src/Version.cs
@@ -1,6 +1,6 @@
 namespace Scorpio {
     public static class Version {
-        public const string version = "1.0.6";
+        public const string version = "1.0.4";
         public const string date = "2018-12-20";
     }
 }
