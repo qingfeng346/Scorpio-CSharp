b23f79903854223ecaba67623c96f18662e48eaa
linyuan.yang <linyuan.yang@centurygame.com>
2019-03-11 18:28:44 +0800

    更新版本到 1.0.7

--- Scorpio/src/Version.cs
+++ Scorpio/src/Version.cs
@@ -1,6 +1,6 @@
 namespace Scorpio {
     public static class Version {
-        public const string version = "1.0.6";
+        public const string version = "1.0.7";
         public const string date = "2019-03-11";
     }
 }
