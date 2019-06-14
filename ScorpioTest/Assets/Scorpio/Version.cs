b73274a237a0f9b092c64222f5c218e2ba9f4182
linyuan.yang <linyuan.yang@centurygame.com>
2019-06-14 15:45:41 +0800

    v2.0.0 preview4

--- ScorpioTest/Assets/Scorpio/Version.cs
+++ ScorpioTest/Assets/Scorpio/Version.cs
@@ -1,6 +1,6 @@
 namespace Scorpio {
     public static class Version {
-        public const string version = "2.0.0-preview3";
+        public const string version = "2.0.0-preview4";
         public const string date = "2019-06-14";
     }
 }
