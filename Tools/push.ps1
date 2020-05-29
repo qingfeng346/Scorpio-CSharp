5bacea5060bfdd7681e73d5cdde04429e57d86fb
linyuan.yang <linyuan.yang@centurygame.com>
2020-05-29 10:53:46 +0800

    push.ps1

--- Tools/push.ps1
+++ Tools/push.ps1
@@ -1,6 +1,5 @@
 $files = [System.IO.Directory]::GetFiles("../bin", "*.nupkg")
 $apiKey = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("b3kyZWJucXlkc3R1MmF5MnFkNXFobHNna29odDNqbnVuZnBhd3A0cGc2cTZuZQ=="))
-Write-Host $apiKey
 foreach ($file in $files) {
     dotnet nuget push $file -k "$apiKey" -s https://api.nuget.org/v3/index.json
 }
