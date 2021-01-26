af3a11e2dbefc660c7c09b1353311e7dc02b7fc8
linyuan.yang <linyuan.yang@centurygame.com>
2021-01-26 18:01:47 +0800

    ReleaseNotes.md

--- ScorpioUnityTest/Assets/Scorpio/Version.cs
+++ ScorpioUnityTest/Assets/Scorpio/Version.cs
@@ -2,6 +2,6 @@
 namespace Scorpio {
     public static class Version {
         public const string version = "2.0.13";
-        public const string date = "2021-01-06";
+        public const string date = "2021-01-26";
     }
 }
