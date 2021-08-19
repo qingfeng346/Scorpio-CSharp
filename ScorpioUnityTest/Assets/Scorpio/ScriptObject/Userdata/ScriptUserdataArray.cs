b1ddf9984eb0934d297987cb7c4e5e6ceadb104f
linyuan.yang <linyuan.yang@centurygame.com>
2021-08-19 10:16:22 +0800

    优化List取值速度

--- ScorpioUnityTest/Assets/Scorpio/ScriptObject/Userdata/ScriptUserdataArray.cs
+++ ScorpioUnityTest/Assets/Scorpio/ScriptObject/Userdata/ScriptUserdataArray.cs
@@ -1,32 +1,33 @@
 using System;
+using System.Collections;
 using Scorpio.Tools;
 
 namespace Scorpio.Userdata {
     /// <summary> 数组 object </summary>
     public class ScriptUserdataArray : ScriptUserdataObject {
-        protected Array m_Array;
+        protected IList m_Array;
         protected Type m_ElementType;
-        public ScriptUserdataArray(Array array, UserdataType type) : base(array, type) {
+        public ScriptUserdataArray(IList array, UserdataType type) : base(array, type) {
             m_Array = array;
             m_ElementType = type.Type.GetElementType();
         }
         public override ScriptValue GetValue(double index) {
-            return ScriptValue.CreateValue(m_Array.GetValue((int)index));
+            return ScriptValue.CreateValue(m_Array[(int)index]);
         }
         public override ScriptValue GetValue(long index) {
-            return ScriptValue.CreateValue(m_Array.GetValue((int)index));
+            return ScriptValue.CreateValue(m_Array[(int)index]);
         }
         public override ScriptValue GetValue(object index) {
-            return ScriptValue.CreateValue(m_Array.GetValue(Convert.ToInt32(index)));
+            return ScriptValue.CreateValue(m_Array[Convert.ToInt32(index)]);
         }
         public override void SetValue(double index, ScriptValue value) {
-            m_Array.SetValue(Util.ChangeType(value, m_ElementType), (int)index);
+            m_Array[(int)index] = Util.ChangeType(value, m_ElementType);
         }
         public override void SetValue(long index, ScriptValue value) {
-            m_Array.SetValue(Util.ChangeType(value, m_ElementType), (int)index);
+            m_Array[(int)index] = Util.ChangeType(value, m_ElementType);
         }
         public override void SetValue(object index, ScriptValue value) {
-            m_Array.SetValue(Util.ChangeType(value, m_ElementType), Convert.ToInt32(index));
+            m_Array[Convert.ToInt32(index)] = Util.ChangeType(value, m_ElementType);
         }
     }
 }
