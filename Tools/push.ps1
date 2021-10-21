7e575fc3bbc122ff2383ce72e0f2b83d97a49f74
linyuan.yang <linyuan.yang@centurygame.com>
2021-10-21 14:15:09 +0800

    push.ps1

--- Tools/push.ps1
+++ Tools/push.ps1
@@ -1,5 +1,5 @@
 $files = [System.IO.Directory]::GetFiles("../bin", "*.nupkg")
-$apiKey = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("b3kyZWJucXlkc3R1MmF5MnFkNXFobHNna29odDNqbnVuZnBhd3A0cGc2cTZuZQ=="))
+$apiKey = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("b3kyZDVxNmEyenZ1enlsZm5rcHFhaGU0eGlxMnJuZHViY2ZuMmQ2MnE3YmoyYQ=="))
 foreach ($file in $files) {
     dotnet nuget push $file -k "$apiKey" -s https://api.nuget.org/v3/index.json
 }
