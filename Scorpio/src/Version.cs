deac5af1913661acfd036004d812ccd803402cb8
while <qingfeng346@fengyuezhu.com>
2021-11-28 20:25:11 +0800

    ReleaseNotes.md

--- Scorpio/src/Version.cs
+++ Scorpio/src/Version.cs
@@ -1,7 +1,7 @@
 //github : https://github.com/qingfeng346/Scorpio-CSharp
 namespace Scorpio {
     public static class Version {
-        public const string version = "2.3.3";
-        public const string date = "2021-11-12";
+        public const string version = "2.3.4";
+        public const string date = "2021-11-28";
     }
 }
