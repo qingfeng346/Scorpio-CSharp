bd2de61108dbd83d959c66bb7e6ce95eb743c668
linyuan.yang <linyuan.yang@centurygame.com>
2021-12-03 13:02:43 +0800

    update

--- Tools/build.ps1
+++ Tools/build.ps1
@@ -1,13 +1,11 @@
 $version = "2.3.5"
 $name = "sco"
-$cur = Get-Location
 
 $today = Get-Date
 $date = $today.ToString('yyyy-MM-dd')
 
 Write-Host "开始打包$name  版本号:$version  日期:$date"
 
-Set-Location ../Scorpio
 $fileData = @"
 //github : https://github.com/qingfeng346/Scorpio-CSharp
 namespace Scorpio {
@@ -17,44 +15,38 @@ namespace Scorpio {
     }
 }
 "@
-Set-Content -Path ./src/Version.cs -Value $fileData -Encoding utf8
+Set-Content -Path ../Scorpio/src/Version.cs -Value $fileData -Encoding utf8
 
-$contextExcute = Get-Content -Path ./src/Runtime/ScriptContextExecute.cs
-Set-Content -Path ./src/Runtime/ScriptContextExecuteBase.cs -Value "#define EXECUTE_BASE",$contextExcute
-Set-Content -Path ./src/Runtime/ScriptContextExecuteCoroutine.cs -Value "#define EXECUTE_COROUTINE",$contextExcute
-Set-Content -Path ./src/Runtime/ScriptContextExecuteCoroutineBase.cs -Value "#define EXECUTE_COROUTINE","#define EXECUTE_BASE",$contextExcute
+$contextExcute = Get-Content -Path ../Scorpio/src/Runtime/ScriptContextExecute.cs
+Set-Content -Path ../Scorpio/src/Runtime/ScriptContextExecuteBase.cs -Value "#define EXECUTE_BASE",$contextExcute
+Set-Content -Path ../Scorpio/src/Runtime/ScriptContextExecuteCoroutine.cs -Value "#define EXECUTE_COROUTINE",$contextExcute
+Set-Content -Path ../Scorpio/src/Runtime/ScriptContextExecuteCoroutineBase.cs -Value "#define EXECUTE_COROUTINE","#define EXECUTE_BASE",$contextExcute
 
 Remove-Item ../bin/* -Force -Recurse
 Write-Host "正在生成nupkg..."
-dotnet pack -p:PackageVersion=$version -o ../bin/ /p:AssemblyVersion=$version | Out-Null
-
-Set-Location ../ScorpioExec
+dotnet pack ../Scorpio/Scorpio.csproj -p:PackageVersion=$version -o ../bin/ /p:AssemblyVersion=$version | Out-Null
 
 $platforms = @("win-x86", "win-x64", "win-arm", "win-arm64", "linux-x64", "linux-musl-x64", "linux-arm", "linux-arm64", "osx-x64", "osx-arm64")
+$aipPath = ".\Install.aip"
 foreach ($platform in $platforms) {
     Write-Host "正在打包 $platform 版本..."
-    $pathName = $name-$platform
-    dotnet publish --self-contained -c release -o ../bin/$pathName -r $platform /p:DefineConstants="SCORPIO_STACK" /p:AssemblyVersion=$version | Out-Null
+    $pathName = "$name-$platform"
+    dotnet publish ../ScorpioExec/ScorpioExec.csproj --self-contained -c release -o ../bin/$pathName -r $platform /p:DefineConstants="SCORPIO_STACK" /p:AssemblyVersion=$version | Out-Null
     Write-Host "正在压缩 $platform ..."
     $fileName = "$name-$version-$platform"
     Compress-Archive ../bin/$pathName/* ../bin/$fileName.zip -Force
-    if ($IsWindows) {
+    if ($IsWindows -and (($platform -eq "win-x86") -or ($platform -eq "win-x64"))) {
+        Write-Host "正在生成安装包 $platform ..."
+        git checkout $aipPath
+        AdvancedInstaller.com /edit $aipPath /AddFolder APPDIR ..\bin\$pathName\
+        AdvancedInstaller.com /edit $aipPath /SetVersion $version
+        AdvancedInstaller.com /edit $aipPath /SetPackageName ..\bin\$fileName.msi -buildname DefaultBuild
         if ($platform -eq "win-x86") {
-            git checkout ./Install.aip
-            AdvancedInstaller.com /edit .\Install.aip /AddFolder APPDIR ..\bin\$pathName\
-            AdvancedInstaller.com /edit .\Install.aip /SetPackageName ..\bin\$fileName.msi -buildname DefaultBuild
-            AdvancedInstaller.com /edit MyProject.aip /SetPackageType x86 DefaultBuild
-            AdvancedInstaller.com /edit .\Install.aip /SetVersion $version
-            AdvancedInstaller.com /build .\Install.aip buildslist DefaultBuild
+            AdvancedInstaller.com /edit $aipPath /SetPackageType x86 -buildname DefaultBuild
         } elseif ($platform -eq "win-x64") {
-            git checkout ./Install.aip
-            AdvancedInstaller.com /edit .\Install.aip /AddFolder APPDIR ..\bin\$pathName\
-            AdvancedInstaller.com /edit .\Install.aip /SetPackageName ..\bin\$fileName.msi -buildname DefaultBuild
-            AdvancedInstaller.com /edit MyProject.aip /SetPackageType x64 DefaultBuild
-            AdvancedInstaller.com /edit .\Install.aip /SetVersion $version
-            AdvancedInstaller.com /build .\Install.aip buildslist DefaultBuild
+            AdvancedInstaller.com /edit $aipPath /SetPackageType x64 -buildname DefaultBuild
         }
+        AdvancedInstaller.com /build $aipPath -buildslist DefaultBuild
     }
 }
-Set-Location $cur
 Write-Host "生成完成"
