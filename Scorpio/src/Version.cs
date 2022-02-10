e586266e1311b7ada08c5b77c7ddb8dda6c3bf59
while <qingfeng346@fengyuezhu.com>
2022-02-10 13:02:56 +0800

    升级版本号

--- Scorpio/src/Version.cs
+++ Scorpio/src/Version.cs
@@ -1,7 +1,7 @@
 //github : https://github.com/qingfeng346/Scorpio-CSharp
 namespace Scorpio {
     public static class Version {
-        public const string version = "2.3.6";
-        public const string date = "2022-02-02";
+        public const string version = "2.3.7";
+        public const string date = "2022-02-10";
     }
 }
