235d53be13f28a86f023c51ed2cfcf9e63455a83
linyuan.yang <linyuan.yang@centurygame.com>
2022-07-21 14:21:05 +0800

    version

--- Scorpio/src/Version.cs
+++ Scorpio/src/Version.cs
@@ -1,7 +1,7 @@
 //github : https://github.com/qingfeng346/Scorpio-CSharp
 namespace Scorpio {
     public static class Version {
-        public const string version = "2.4.1";
-        public const string date = "2022-07-20";
+        public const string version = "2.3.10";
+        public const string date = "2022-07-21";
     }
 }
