530999dbc808b054ab1095fe28ff81c892fd1f79
linyuan.yang <linyuan.yang@centurygame.com>
2023-05-18 10:22:59 +0800

    fix

--- Scorpio/src/Instruction/ScriptInstruction.cs
+++ Scorpio/src/Instruction/ScriptInstruction.cs
@@ -4,7 +4,7 @@ namespace Scorpio.Instruction {
     //不能使用 struct 编译时会稍后修改内部值 
     //单条指令
     [StructLayout(LayoutKind.Explicit, Pack = 1)]
-    public struct ScriptInstruction {
+    public class ScriptInstruction {
         [FieldOffset(0)] public int opvalue;         //指令值
         [FieldOffset(4)] public ushort line;         //代码在多少行
         [FieldOffset(6)] public Opcode opcode;       //指令类型
